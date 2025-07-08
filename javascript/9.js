f8075730cb4307111395a5642eb16d2e1a85d95e
A511-git <mhdatif0511@gmail.com>
2025-07-08 13:24:53 +0530

    faulty calc

--- javascript/9.js
+++ javascript/9.js
@@ -1,1 +1,35 @@
-let 
+let num1 = parseFloat(prompt("Enter number 1 : "));
+let num2 = parseFloat(prompt("Enter number 2 : "));
+let operator = prompt("Enter operator : ");
+
+alert(`Number 1 : ${num1} \nNumber 2 : ${num2} \nOperator : ${operator}`);
+
+if (Math.random() <= 0.1) {
+    if (operator == '+') {
+        alert(`Mischeviously changed operator from ${operator} to -`);
+        operator = '-';
+    }
+    if (operator == '*') {
+        alert(`Mischeviously changed operator from ${operator} to +`);
+        operator = '+';
+    }
+    if (operator == '-') {
+        alert(`Mischeviously changed operator from ${operator} to /`);
+        operator = '/';
+    }
+    if (operator == '/') {
+        alert(`Mischeviously changed operator from ${operator} to *`);
+        operator = '*';
+    }
+}
+
+let result;
+switch (operator) {
+    case '+': result = num1 + num2; break;
+    case '*': result = num1 * num2; break;
+    case '-': result = num1 - num2; break;
+    case '/': result = num2==0 ? "Can't devide by Zero" : num1 / num2; break;
+    default : result="Invalid Operator"; break;
+}
+
+alert(`Result \n ${num1} ${operator} ${num2} = ${result}`);
